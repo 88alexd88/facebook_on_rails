440d5908e73521109906579398b2fe67d0e0577d
Alexander D'Angelo <alexdangelo88@gmail.com>
2014-07-01 13:39:01 -0400

    fixed bug for validating though facebook only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,14 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_filter :configure_permited_parameters, if :devise_controller?
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
-  def configurer_permitted_parameters
-    devise_parameter_sanitizer.for{:sign_up} do |u|
-      u.permit(
-        :email,
-        :name,
-        :passwords,
-        :password_confirmation
-        )
-    end
+  def configure_permitted_parameters
 
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit( :email, :name, :passwords, :password_confirmation )
+    end
 
     devise_parameter_sanitizer.for(:account_update) do |u|
       u.permit(
@@ -23,6 +18,8 @@ class ApplicationController < ActionController::Base
           :passwords,
           :password_confirmation
           )
-      end
+    end
+
   end
+
 end
