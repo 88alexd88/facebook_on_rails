0d454fdebc0e6f4cc6f1ffb3a25dd425792a919f
Alexander D'Angelo <alexdangelo88@gmail.com>
2014-07-01 11:51:26 -0400

    edited application controller. configured omniauth routes. added users omniauth controller, added header link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :configure_permited_parameters, if :devise_controller?
   protect_from_forgery with: :exception
+
+  def configurer_permitted_parameters
+    devise_parameter_sanitizer.for{:sign_up} do |u|
+      u.permit(
+        :email,
+        :name,
+        :passwords,
+        :password_confirmation
+        )
+    end
+
+
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(
+          :current_password,
+          :email,
+          :name,
+          :passwords,
+          :password_confirmation
+          )
+      end
+  end
 end
